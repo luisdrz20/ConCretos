046b4c70e517a4ba9e503fede2722ae3e0dc93f9
luisdrz20 <luisdavidrs002@gmail.com>
2024-03-01 22:51:18 -0600

    Mensaje

---
+++ ConCretos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConCretos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
